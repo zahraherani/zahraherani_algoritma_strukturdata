969839a126f4e8337b946ee02f2598999494f07d
zahraherani <marukochan097@gmail.com>
2024-09-27 10:54:29 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,13 @@
+
+nama_depan = "zahra"
+nama_belakang = "herani"
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = '18'
+alamat = 'bangka'
+kata_mutiara = "Dont judge a abook it's by cover"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara', type(kata_mutiara))
